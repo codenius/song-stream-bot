3d5bcf788568e4bf9150ce1c45cdea7a8228967a
codenius <94353572+codenius@users.noreply.github.com>
2023-10-01 20:35:34 +0000

    feat: update status message while fetching

--- lib/bot.ts
+++ lib/bot.ts
@@ -5,6 +5,7 @@ import {
   conversations,
   createConversation,
 } from "grammy_conversations";
+import { hydrate, HydrateFlavor } from "@grammyjs/hydrate";
 import { downloadSongs } from "./downloadSongs.ts";
 import { sendSongs } from "./sendSongs.ts";
 import { Router } from "oak";
@@ -22,10 +23,10 @@ interface SessionData {
   spotifyLink: string;
   oldSongs: Song[];
 }
-export type MyContext =
-  & Context
-  & SessionFlavor<SessionData>
-  & ConversationFlavor;
+export type MyContext = Context &
+  SessionFlavor<SessionData> &
+  ConversationFlavor &
+  HydrateFlavor<Context>;
 type MyConversation = Conversation<MyContext>;
 
 const bot = new Bot<MyContext>(BOT_TOKEN);
@@ -35,6 +36,7 @@ function initial(): SessionData {
 }
 bot.use(session({ initial, storage: await supabaseSessionStore() }));
 bot.use(conversations());
+bot.use(hydrate());
 
 async function getSpotifyLink(conversation: MyConversation, ctx: MyContext) {
   await ctx.reply("Send a spotify link to watch!");
@@ -52,6 +54,20 @@ await bot.api.setMyCommands([
   { command: "reset", description: "Resets allready send songs list" },
 ]);
 
+async function createStatusMessage(ctx: MyContext, initialStatus: string) {
+  const statusMessage = await ctx.reply(initialStatus);
+
+  async function setStatus(status: string) {
+    await statusMessage.editText(status);
+  }
+
+  async function deleteStatus() {
+    await statusMessage.delete();
+  }
+
+  return [setStatus, deleteStatus];
+}
+
 bot.command("start", (ctx) => {
   ctx.reply("Hi there!");
 });
@@ -60,31 +76,44 @@ bot.command("set", async (ctx) => {
 });
 bot.command("link", (ctx) => {
   ctx.reply(
-    ctx.session.spotifyLink || "No spotify link stored. Set one with /set.",
+    ctx.session.spotifyLink || "No spotify link stored. Set one with /set."
   );
 });
 bot.command("fetch", async (ctx) => {
+  const [setStatus, deleteStatus] = await createStatusMessage(
+    ctx,
+    "Fetch triggered."
+  );
+
   if (ctx.session.spotifyLink) {
-    await ctx.reply("Fetching new songs...");
+    await setStatus("Fetching new songs...");
     const songsToDownload = await fetchSongs(ctx.session.spotifyLink, ctx);
     if (songsToDownload.length) {
-      ctx.reply(`${songsToDownload.length} new songs found. Downloading...`);
+      await setStatus(
+        `${songsToDownload.length} new songs found. Downloading...`
+      );
       await downloadSongs(songsToDownload);
-      ctx.reply("Download completed.");
+      await setStatus("Sending songs now...");
+      await sendSongs(songsToDownload, ctx);
+      await setStatus("Done.");
     } else {
-      ctx.reply("No new songs found.");
-      return;
+      await setStatus("No new songs found.");
     }
-    ctx.reply("Sending songs now...");
-    await sendSongs(songsToDownload, ctx);
   } else {
-    ctx.reply("No spotify link stored. Set one with /set.");
+    await setStatus("No spotify link stored. Set one with /set.");
   }
+  setTimeout(
+    () =>
+      deleteStatus().catch((e) => {
+        console.error(e);
+      }),
+    5000
+  );
 });
 bot.command("reset", (ctx) => {
   ctx.session.oldSongs = [];
   ctx.reply(
-    "Successfully reset allready sent songs list. A new fetch will re-download all songs.",
+    "Successfully reset allready sent songs list. A new fetch will re-download all songs."
   );
 });
 
